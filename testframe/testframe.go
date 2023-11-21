d3c740980d6ad1b191b56ee8b99f3787ec18a580
Karel Kubat <karel@kubat.nl>
2023-11-21 18:46:56 +0100

    extended tests

--- testframe/testframe.go
+++ testframe/testframe.go
@@ -40,23 +40,31 @@ func Make(src string) error {
 	if err != nil {
 		return fmt.Errorf("failed to read %v: %v", src, err)
 	}
-	packageName := ""
-	for _, line := range strings.Split(string(content), "\n") {
-		if strings.HasPrefix(line, "package") {
-			parts := strings.Split(line, " ")
-			if len(parts) != 2 {
-				return fmt.Errorf("package statement %q must have just 2 parts", line)
-			}
-			packageName = parts[1]
-			break
-		}
+	pkgame, err := packageName(content, src)
+	if err != nil {
+		return err
 	}
-	if packageName == "" {
-		return fmt.Errorf("failed to extract package name from %v", src)
-	}
-	if err = os.WriteFile(tst, []byte(fmt.Sprintf(testFrame, packageName)), 0644); err != nil {
+	if err = os.WriteFile(tst, []byte(fmt.Sprintf(testFrame, pkgame)), 0644); err != nil {
 		return fmt.Errorf("failed to write %v: %v", tst, err)
 	}
 
 	return nil
+}
+
+func packageName(content []byte, fname string) (string, error) {
+	pkgame := ""
+	for _, line := range strings.Split(string(content), "\n") {
+		if strings.HasPrefix(line, "package") {
+			parts := strings.Split(line, " ")
+			if len(parts) != 2 {
+				return "", fmt.Errorf("package statement %q must have just 2 parts", line)
+			}
+			pkgame = parts[1]
+			break
+		}
+	}
+	if pkgame == "" {
+		return "", fmt.Errorf("failed to extract package name from %v", fname)
+	}
+	return pkgame, nil
 }
--- testframe/testframe_test.go
+++ testframe/testframe_test.go
@@ -1,9 +1,42 @@
 package testframe
 
 import (
+	"strings"
 	"testing"
 )
 
-func TestAll(t *testing.T) {
-	// TODO: Add tests
+func TestPackageName(t *testing.T) {
+	for _, test := range []struct {
+		content     string
+		wantErr     string
+		wantPackage string
+	}{
+		{
+			content:     "package foo",
+			wantErr:     "",
+			wantPackage: "foo",
+		},
+		{
+			content:     "package foo bar",
+			wantErr:     "must have just 2 parts",
+			wantPackage: "",
+		},
+		{
+			content:     "foo bar",
+			wantErr:     "failed to extract package name",
+			wantPackage: "",
+		},
+	} {
+		gotPackage, err := packageName([]byte(test.content), "src.go")
+		switch {
+		case err == nil && test.wantErr != "":
+			t.Errorf("packagename(%q,_) = nil, want error with %q", test.content, test.wantErr)
+		case err != nil && test.wantErr == "":
+			t.Errorf("packagename(%q,_) = _,%q, want nil error", test.content, err.Error())
+		case err != nil && test.wantErr != "" && !strings.Contains(err.Error(), test.wantErr):
+			t.Errorf("packagename(%q,_) = _,%q, want error with %q", test.content, err.Error(), test.wantErr)
+		case err == nil && test.wantErr == "" && gotPackage != test.wantPackage:
+			t.Errorf("packagename(%q,_) = %q,_, want package %q", test.content, gotPackage, test.wantPackage)
+		}
+	}
 }
