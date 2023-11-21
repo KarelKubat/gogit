f6f38adafd4c704b349cc5e208bde7f679f7d759
Karel Kubat <karel@kubat.nl>
2023-11-21 17:55:05 +0100

    extended tests

--- action/action_test.go
+++ action/action_test.go
@@ -4,6 +4,11 @@ import (
 	"testing"
 )
 
-func TestAll(t *testing.T) {
-	// TODO: add tests
+func TestSuggest(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		Suggest("this is suggestion %v", i)
+	}
+	if slen := len(suggestions); slen != 10 {
+		t.Errorf("len(suggestions) = %v, want 10", slen)
+	}
 }
--- run/run_test.go
+++ run/run_test.go
@@ -4,6 +4,27 @@ import (
 	"testing"
 )
 
-func TestAll(t *testing.T) {
-	// TODO: add tests
+func TestExec(t *testing.T) {
+	for _, test := range []struct {
+		cmd           []string
+		wantErr       bool
+		wantCacheSize int
+	}{
+		{
+			cmd:           []string{"ls"},
+			wantErr:       false,
+			wantCacheSize: 1,
+		},
+	} {
+		_, err := Exec("", test.cmd)
+		gotErr := err != nil
+		if gotErr != test.wantErr {
+			t.Errorf("Exec(_,%v) = _,%q, goterr=%v, wanterr=%v", test.cmd, err, gotErr, test.wantErr)
+		}
+		if !gotErr {
+			if gotCacheSize := len(cache); gotCacheSize != test.wantCacheSize {
+				t.Errorf("len(cache) = %v, want %v", gotCacheSize, test.wantCacheSize)
+			}
+		}
+	}
 }
